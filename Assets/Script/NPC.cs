19891981f20f989af8a7691fdee78329707b14fb
CowBroth <535945@escg.ac.uk>
2026-04-14 11:25:43 +0100

    uuh

--- Assets/Script/NPC.cs
+++ Assets/Script/NPC.cs
@@ -5,6 +5,6 @@ public class NPC : MonoBehaviour
     public Dialogue dialogue;
     public void TriggerDialogue()
     {
-
+        FindAnyObjectByType<DialogueManager>().StartDialogue(dialogue);
     }
 }
--- Assets/Script/TestMoveScript.cs
+++ Assets/Script/TestMoveScript.cs
@@ -4,18 +4,39 @@ using UnityEngine.InputSystem;
 public class TestMoveScript : MonoBehaviour
 {
     public float m_speed;
+    bool interactButton;
     private InputAction m_move;
+    private InputAction p_interact;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_move = InputSystem.actions.FindAction("Move");
+        p_interact = InputSystem.actions.FindAction("Interact");
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 moveValue = m_move.ReadValue<Vector2>();
+        interactButton = p_interact.IsPressed();
         transform.Translate(moveValue * m_speed * Time.fixedDeltaTime);
-        Debug.Log(moveValue);
+
+        if (interactButton)
+        {
+            FindAnyObjectByType<DialogueManager>().DisplayNextSentence();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D npc)
+    {
+        if (interactButton && npc.gameObject.CompareTag("Friendly"))
+        {
+            npc.gameObject.GetComponent<NPC>().TriggerDialogue();
+        }
     }
 }
